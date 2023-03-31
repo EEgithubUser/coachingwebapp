70ad53d20ed9dca2f9cbfce4bd9349d2ff270126
Kevin <kevchan129@gmail.com>
2023-03-30 22:09:15 -0700

    Implement gunicorn and add .env file for SECRET_KEY

--- website/__init__.py
+++ website/__init__.py
@@ -3,21 +3,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-from configparser import ConfigParser
-
-file = 'config.ini'
-config = ConfigParser()
-config.read(file)
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+from decouple import config
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-# Create flask applicaton, initialize secret key, return it from the function
-def create_app():
+def create_app(db_url=None):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = config['SECRET_KEY']['key']
+    load_dotenv()
+    app.config['SECRET_KEY'] = config("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     from .views import views
     from .auth import auth
@@ -26,8 +25,6 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Weight, Note
-
-    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -38,10 +35,5 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
 
 
